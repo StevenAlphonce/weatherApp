61cf07b18598b09b583c22c3cc325aea83e3edf1
alphoncesteven <alphoncesteven@gmail.com>
2023-06-15 12:06:12 +0300

    The Initial complete weatherApp

---
+++ app/src/main/java/com/example/weatherapp/cityFinder.java
@@ -0,0 +1,38 @@
+package com.example.weatherapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class cityFinder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_city_finder);
+        final EditText editText=findViewById(R.id.searchCity);
+
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                String newCity= editText.getText().toString();
+                Intent intent=new Intent(cityFinder.this,MainActivity.class);
+                intent.putExtra("City",newCity);
+                startActivity(intent);
+
+
+
+                return false;
+            }
+        });
+
+
+
+
+
+    }
+}
